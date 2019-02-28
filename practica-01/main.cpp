485078ae149b2e97b5ed1800842d40e656463b1b
Felipe De Jesus Ruiz Garcia <felipe.de.jesus.ruiz.garcia@intel.com>
2019-02-28 15:56:40 -0600

    Prevent close with Ctrl + c
    Signed-off-by: Felipe De Jesus Ruiz Garcia <felipe.de.jesus.ruiz.garcia@intel.com>

--- practica-01/main.cpp
+++ practica-01/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <cstdio>
 #include <list>
+#include <csignal>
 
 #include "menu.hpp"
 
 using namespace std;
 
+void signalHandler( int signum  = 0 ) {
+
+}
+
 int main(){
+
+	signal(SIGINT, signalHandler);
+
 	Menu mainMenu = Menu();
 	mainMenu.mainMenu();
 
