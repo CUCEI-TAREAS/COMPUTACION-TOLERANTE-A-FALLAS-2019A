c1fbdd6a100dcbc0ef9af739649e49beb824ea5a
Felipe de Jesus Ruiz Garcia <me@tranzemc.com>
2019-03-24 01:32:07 -0400

    Prevent close with Ctrl + z and another signals
    Signed-off-by: Felipe de Jesus Ruiz Garcia <me@tranzemc.com>

--- practica-01/main.cpp
+++ practica-01/main.cpp
@@ -14,6 +14,9 @@ void signalHandler( int signum  = 0 ) {
 int main(){
 
 	signal(SIGINT, signalHandler);
+	signal(SIGABRT, signalHandler);
+	signal(SIGSTOP, signalHandler);
+	signal(SIGTSTP, signalHandler);
 
 	Menu mainMenu = Menu();
 	mainMenu.mainMenu();
